5fa963cf2298703aa9984f7a2d993343dbdb4a7d
stephy0410 <stephyborrego04@gmail.com>
2025-05-05 11:43:37 -0600

    Base de datos con favoritos

--- BACKEND/models/user.js
+++ BACKEND/models/user.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema({
     createdAt: { 
         type: Date, 
         default: Date.now 
+    },
+    favoritos:{
+        type:[String],
+        default: []
     }
 });
 
